b1af02761d255fc29a353690f09212c125407541
The-Reverent-Green <132163747+The-Reverent-Green@users.noreply.github.com>
2025-11-18 12:06:51 +0000

    Don't like structurizr
    Played with structurizr, I think pen and paper is faster and freer.

--- C4_models.py
+++ C4_models.py
@@ -1,7 +1,33 @@
 '''
 This file is used to model the C4 models.
 '''
+from pystructurizr.dsl import Workspace
 
-from pystructurizr import Workspace, Model, ViewSet
-from pystructurizr.model import Person, SoftwareSystem
-from pystructurizr.view import SystemContextView
+'''bash cmds
+pystructurizr dump --view this_filename > output.dsl
+pystructurizr dump --view C4_models > CMS_Context.dsl
+'''
+#Context C4 Level 1
+with Workspace() as workspace:
+    with workspace.Model(name="Context") as model:
+        #Define the users
+        consumer = model.Person("Consumer")
+        agent = model.Person("Help Desk Agent")
+        manager = model.Person("Help desk Manager")
+        supporter = model.Person("Support Agent")
+        sysAd = model.Person("System Administrator")
+
+        with model.SoftwareSystem("CMS") as software_system:
+            webapp = software_system.Container("Frontend")
+            api = software_system.Container("API")
+            db = software_system.Container("Database")
+        
+        consumer.uses(webapp, "Uses")
+        webapp.uses(api, "Calls", "HTTPS")
+        api.uses(db, "Reads/writes", "SQL")
+    
+    workspace.ContainerView(
+        software_system,
+        "Container View",
+        "Container diagram showing system components"
+    )
